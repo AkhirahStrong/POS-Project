3e4a0aa553ae4ac8eb67ab7ed4ef6493706db3e3
Akhirahstrong <codeclubdetroit@gmail.com>
2023-05-24 20:05:21 -0400

    Add array

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ document.querySelector(".menuIcon").addEventListener("click", function (e) {
     menuItems.classList.add("show-menu");
   } else {
     menuItems.style.display = "none";
-    // menuItems.style.display = "none";
-    // menuItems.classList.remove("show-menu"); // Remove class for animation
-    // setTimeout(function () {
-    //   menuItems.style.display = "none";
-    // }, 300);
   }
 });
+
+let cars = [
+  {
+    make: "Ford",
+    model: "Taurus",
+    year: 2013,
+    color: "Black",
+    price: 12000,
+    available: true,
+  },
+];
+
+document.getElementById("search-container").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(event.target);
+  const make = data.get("make");
+  const model = data.get("model");
+  console.log(make + model);
+});
+
+function findCarByName(array, make, model) {}
